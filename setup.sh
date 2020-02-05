9546aa6c65cb9ef95d23636aaa2758860c3b4dfa
Eric <42959685+lantange@users.noreply.github.com>
2020-02-06 00:06:01 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 
 mkdir src/api
 cd src/api
-wget -q https://raw.githubusercontent.com/lantange/BareMetal-kernel/master/api/libBareMetal.asm 
+wget -q https://raw.githubusercontent.com/lantange/BareMetal/master/api/libBareMetal.asm 
 cd ../..
 
 mkdir bin
